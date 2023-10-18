38101e0a93ed29094a59af0ace04ca2a7a851a5d
sahill-1 <sv260397@gmail.com>
2023-10-18 11:25:54 +0530

    completed todo list with just remove button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import React, {useState} from 'react';
+import { connect } from 'react-redux';
+import { addTask, removeTask } from './taskActions';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div>Hello</div>
+const App = ({tasks, addTask, removeTask}) => {
+  const [taskText, setTaskText] = useState("");
+
+  const handleAddTask = () => {
+    if(taskText.trim() !== ""){
+      addTask({id:Date.now(), text:taskText});
+      setTaskText("")
+    }
+  };
+  const handleRemoveTask = taskId => {
+    removeTask(taskId)
+  };
+
+  return(
+    <div className='App'>
+      <h1>Redux Store</h1>
+      <div>
+        <input
+          type='text'
+          value={taskText}
+          onChange={e => setTaskText(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.text}{" "}
+            <button onClick={() => handleRemoveTask(task.id)}>Remove</button>
+          </li>
+        )
+        )}
+      </ul>
     </div>
-  );
+  )
+}
+const mapStateToProps = state => {
+  return{
+    tasks:state.tasks
+  }
 }
 
-export default App;
+const mapDispatchToProps = {
+  addTask,
+  removeTask
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
